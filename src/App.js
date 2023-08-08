97b75693fc5a146a38e5eb77cb655a908b57a345
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2023-08-07 21:30:57 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={mba} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to 2023 - MBA SJC</h1>
+          <h1 className="App-title">Welcome to 2023 MBA SJC</h1>
         </header>
       </div>
     );
